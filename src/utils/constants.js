408edae74171f1d6698151604f8e96fef167bd4c
unknown <singhshivansh12@gmail.com>
2023-02-28 00:26:24 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // const API_KEY = "AIzaSyAKvQ-QvF3YYFw5OOa9C42VWBxqM2j8j0w";
-// const API_KEY = "AIzaSyBop-NQnholFCeyJE88uaNDp_bBVC3blIw";
-const API_KEY = '';
+const API_KEY = "AIzaSyBop-NQnholFCeyJE88uaNDp_bBVC3blIw";
+// const API_KEY = '';
 
 export const YOUTUBE_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=AU&key=' + API_KEY;
 export const RRELATED_API = 'https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=[VIDEO_ID]&maxResults=50&type=video&key=' + API_KEY;
